10081617b6fe58819e51246b55514057d2293dac
dtrzes <112385331+dtrzes@users.noreply.github.com>
2022-09-06 15:13:25 +0200

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text( 🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text( 🥑🍞'avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text( '🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text( '🥑🍞avocado Toast')
