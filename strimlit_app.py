0c053ee34f61ed6a15d1a16248a98a7bf2d1b99b
dtrzes <112385331+dtrzes@users.noreply.github.com>
2022-09-07 06:03:00 +0200

    Update strimlit_app.py
    typo

--- strimlit_app.py
+++ strimlit_app.py
@@ -12,6 +12,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe (my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick Some Favoruite Fruits:', list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
 
                       #display a table on the page1
