572e24239e342c9edc3bd96a97626cea52a9899f
dtrzes <112385331+dtrzes@users.noreply.github.com>
2022-09-12 08:34:38 +0200

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -28,17 +28,28 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-#import requests --> move it to top of the screen
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_chose:
+      streamlit.error ("Please select a fruit to get information.")
+  else:
+      #import requests --> move it to top of the screen
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # write your own comment -what does the next line do? - normalize json file
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do? output screen as a table
+      streamlit.dataframe(fruityvice_normalized)      
+except URLerror as e:
+  Streamlit.error()
+      
+      streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 # write your own comment -what does the next line do? - normalize json file
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do? output screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
 
 #don`t run anything past here while we troubleshoot
 streamlit.stop()
