3d809da6125423ca4e95b46dab9fd75c882b64a6
dtrzes <112385331+dtrzes@users.noreply.github.com>
2022-09-12 11:47:17 +0200

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -53,7 +53,7 @@ except URLError as e:
 #streamlit.stop()
 
 #import snowflake.connector --> move it to the top of screen
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your favorite:")
 #Snowflake related functions
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
