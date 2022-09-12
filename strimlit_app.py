485ee77b30bbf9e3349add8ca75cb8b0a81d4963
dtrzes <112385331+dtrzes@users.noreply.github.com>
2022-09-12 09:14:33 +0200

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -42,9 +42,9 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      back from function = get_fruityvice_data(fruit_choice)
+      back_from_function = get_fruityvice_data(fruit_choice)
       # write your own comment - what does this do? output screen as a table
-      streamlit.dataframe (back from function)
+      streamlit.dataframe (back_from_function)
       
 except URLError as e:
   Streamlit.error()
